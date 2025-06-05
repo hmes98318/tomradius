5b84672cdb1935ddceedda5bffde4431097c6962
hmes98318 <hmes98318@gmail.com>
2025-06-05 09:36:01 +0800

    chore(lint): Update ESLint dependencies

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+    {
+        ignores: [
+            ".github/",
+            "dist/",
+            "node_modules/",
+            "public/",
+            "server/",
+            "views/",
+            "*.d.ts"
+        ]
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+        plugins: { js },
+        extends: ["js/recommended"]
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+        languageOptions: { globals: globals.browser }
+    },
+    tseslint.configs.recommended,
+    pluginVue.configs["flat/essential"],
+    {
+        files: ["**/*.vue"],
+        languageOptions: { parserOptions: { parser: tseslint.parser } }
+    },
+    {
+        rules: {
+            'no-unused-vars': "warn",
+            "vue/no-unused-vars": "off",
+            "vue/require-v-for-key": "off",
+            "semi": ["error", "always"]
+        }
+    },
+]);
